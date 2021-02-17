8995f313c512595a5f1b4be1a78ba9f3cb23bc6c
tzvaita <tzvaita@gmail.com>
2021-02-17 15:15:40 +0200

    Add jwt auth to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+          :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :novels, dependent: :destroy
   has_many :likes, class_name: "Favorite",
                     foreign_key: "liker_id",
                     dependent: :destroy
   has_many :liking, through: :likes, source: :liked
+
 end
